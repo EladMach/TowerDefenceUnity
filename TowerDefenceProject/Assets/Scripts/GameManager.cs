11eb78ec7ee6dc1d6357997b3c3245eaa3f67118
Elad Machluf <eladmahluf@gmail.com>
2023-01-07 08:29:00 +0200

    Project Files

--- TowerDefenceProject/Assets/Scripts/GameManager.cs
+++ TowerDefenceProject/Assets/Scripts/GameManager.cs
@@ -5,18 +5,23 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Transforms")]
     public Transform _enemyPrefab;
     public Transform _spawnPoint;
-    
+
+    [Header("Variables")]
     public float _timeBetweenWaves = 5f;
     private float _countDown = 2f;
+    public int _score;
+    private int _waveIndex = 0;
 
+    [Header("Text UI")]
     public TextMeshProUGUI waveCountdownText;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI waveNumText;
 
-    private int _waveIndex = 0;
-    public int _score;
+    
+    
 
     private void Start()
     {
@@ -26,21 +31,11 @@ public class GameManager : MonoBehaviour
     }
     private void Update()
     {
-        if (_countDown <= 0f)
-        {
-            StartCoroutine(SpawnWave());
-            _countDown = _timeBetweenWaves;
-        }
-
-        _countDown -= Time.deltaTime;
-
-        waveCountdownText.text = Mathf.Round(_countDown).ToString();
-        scoreText.text = "Score: " + _score.ToString();
-        waveNumText.text = "Wave: " + _waveIndex.ToString();
+        Countdown();
+        TextUI();
 
         _score = (Mathf.Clamp(_score, 0, 100));
-        
-       
+    
     }
 
     IEnumerator SpawnWave()
@@ -60,12 +55,27 @@ public class GameManager : MonoBehaviour
         Instantiate(_enemyPrefab, _spawnPoint.position, _spawnPoint.rotation);
     }
 
-    
+    void Countdown()
+    {
+        if (_countDown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            _countDown = _timeBetweenWaves;
+        }
+
+        _countDown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Round(_countDown).ToString();
+    }
 
     public void ScoreSystem()
     {
         _score = _score + 1;
     }
 
-    
+    void TextUI()
+    {
+        scoreText.text = "Score: " + _score.ToString();
+        waveNumText.text = "Wave: " + _waveIndex.ToString();
+    }
 }
